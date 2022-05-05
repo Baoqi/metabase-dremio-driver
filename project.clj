740d697590c5598934d5826f4315c826e252d92d
auyer <rafael@rcpassos.me>
2022-05-05 15:32:45 -0300

    Update dremio jdbc

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :min-lein-version "2.5.0"
 
   :dependencies
-  [[com.dremio.distribution/dremio-jdbc-driver "17.0.0-202107060524010627-31b5222b"]]
-  
+  [[com.dremio.distribution/dremio-jdbc-driver "21.1.1-202204292111390812-57b1832f"]]
+
   :aot :all     ; Checks for compile-time failures when building the uberjar
 
   :repositories {"dremio-free" "https://maven.dremio.com/free/"}
@@ -12,8 +12,7 @@
   {:provided
    {:dependencies
     [[org.clojure/clojure "1.10.1"]
-     [metabase-core "1.0.0-SNAPSHOT"]
-     ]}
+     [metabase-core "1.0.0-SNAPSHOT"]]}
 
    :uberjar
    {:auto-clean    true
